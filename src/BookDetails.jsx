0205b28a28e1d911d30ef9ef23adf41cf43a2e30
Vivek Joshi <vivekjoshi@topsinfosolutions.com>
2023-05-28 14:26:10 +0530

    Book Cover image added.

--- src/BookDetails.jsx
+++ src/BookDetails.jsx
@@ -1,3 +1,4 @@
+import { auto } from "@popperjs/core";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -15,7 +16,7 @@ const BookDetails = () => {
   useEffect(() => {
     const handleShowBookDetails = async (id) => {
       setSpinning(true);
-      const url = `http://localhost:8000/api/v1/book/${id}`;
+      const url = `${import.meta.env.VITE_APP_API_URL}/book/${id}`;
 
       const res = await fetch(url, config);
       const response = await res.json();
@@ -28,6 +29,7 @@ const BookDetails = () => {
         genre: bookData.genre.name,
         description: bookData.description,
         isbn: bookData.isbn,
+        cover: bookData.cover,
         published: bookData.published,
         publisher: bookData.publisher.name,
       };
@@ -40,49 +42,59 @@ const BookDetails = () => {
   }, []);
 
   return (
-    <div className="container-fluid">
+    <div className="container">
       <div
-        className="d-flex justify-content-center align-items-center px-4 mb-3"
-        style={{ backgroundColor: "#e6e6fa" }}
+        className="d-flex justify-content-center align-items-center px-4"
+        style={{ backgroundColor: "#781A75" }}
       >
         <h1 className="rounded p-2">Book Details</h1>
       </div>
       {!isSpinning ? (
-        <div className="container-fluid">
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="row">Title</th>
-                <td className="text-start">{bookDetails.title}</td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">Author</th>
-                <td className="text-start">{bookDetails.author}</td>
-              </tr>
-              <tr>
-                <th scope="row">Genre</th>
-                <td className="text-start">{bookDetails.genre}</td>
-              </tr>
-              <tr>
-                <th scope="row">Description</th>
-                <td className="text-start">{bookDetails.description}</td>
-              </tr>
-              <tr>
-                <th scope="row">ISBN</th>
-                <td className="text-start">{bookDetails.isbn}</td>
-              </tr>
-              <tr>
-                <th scope="row">Published</th>
-                <td className="text-start">{bookDetails.published}</td>
-              </tr>
-              <tr>
-                <th scope="row">Publisher</th>
-                <td className="text-start">{bookDetails.publisher}</td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="container d-flex justify-content-center py-3" style={{ backgroundColor: '#DBE3E1' }}>
+          <div className="col-lg-8">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="row">Title</th>
+                  <td className="text-start">{bookDetails.title}</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Author</th>
+                  <td className="text-start">{bookDetails.author}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Genre</th>
+                  <td className="text-start">{bookDetails.genre}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Description</th>
+                  <td className="text-start">{bookDetails.description}</td>
+                </tr>
+                <tr>
+                  <th scope="row">ISBN</th>
+                  <td className="text-start">{bookDetails.isbn}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Published</th>
+                  <td className="text-start">{bookDetails.published}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Publisher</th>
+                  <td className="text-start">{bookDetails.publisher}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="col-lg-4">
+            <img
+              src={`${import.meta.env.VITE_APP_IMG_PATH}/${bookDetails.cover}`}
+              alt=""
+              srcSet=""
+              style={{ height: 350, width: auto }}
+            />
+          </div>
         </div>
       ) : (
         <div className="d-flex align-items-center justify-content-center mt-5">
